8b133ae0241fa1bbe5618bd2c2c12c756b430480
michaeloye <michaeloye23@gmail.com>
2024-04-13 22:37:16 +0100

    Add icon

---
+++ src/components/icons/DropdownIcon.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <path fill="currentColor" fill-rule="evenodd"
+      d="M12.707 14.707a1 1 0 0 1-1.414 0l-5-5a1 1 0 0 1 1.414-1.414L12 12.586l4.293-4.293a1 1 0 1 1 1.414 1.414l-5 5Z"
+      clip-rule="evenodd" />
+  </svg>
+</template>
